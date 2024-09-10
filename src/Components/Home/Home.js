fbeca6e56048cb0955b870cbec105d4cb1fd6761
youcef <abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.com>
2024-09-10 05:55:20 -0700

    home-done

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -62,6 +62,8 @@ const Home = () =>{
                 }
             </div>
         </section>
+
+
     </Fragment>
     );
 }
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -22,14 +22,14 @@ const Layout = ({ }) =>{
                 <Link className="link">Home</Link>
                 <a className="link" href="#products">Shope</a>
                 <Link className="link">About us</Link>
-                <Link className="link">Contact Us</Link>
+                <a className="link" href="#contact">Contact Us</a>
                 <Link className="link">Blog</Link>
                 <a className=" aa" onClick={handledisplay}><i className='bx bx-list-ul'></i></a>
             </nav>
             <div className="cart">
                 <Link className="link2"><i className='bx bx-cart' ></i></Link>
                 <Link className="link2"><i className='bx bx-log-in-circle'></i></Link>
-                <Link className="link2"><i className='bx bxs-store'></i></Link>
+                <a className="link2" href="#products"><i className='bx bxs-store'></i></a>
                 
                
             </div>
@@ -49,7 +49,39 @@ const Layout = ({ }) =>{
             <Outlet/>
         </main>
 
-
+        <section className="footer" id="contact">
+            <div className="first-part">
+                <div className='titlelg'>
+                    <h1>
+                    Luxe Scents
+                    </h1>
+                </div>
+                <div>
+                    <h4>
+                    Welcome to our exclusive collection of perfumes
+                    Indulge in the luxury of our premium perfumes, 
+                    crafted with the finest ingredients from around the
+                    world. Each bottle is a masterpiece, designed to 
+                    captivate your senses and leave a lasting impression.
+                     Experience the essence of elegance
+                    </h4>
+                </div>
+                <div className="contacts">
+                    <a><i className='bx bxl-facebook-square' ></i></a>
+                    <a><i className='bx bxl-instagram' ></i></a>
+                    <a><i className='bx bxl-blogger' ></i></a>
+                    <a><i className='bx bxl-pinterest-alt'></i></a>
+                    <a><i className='bx bxs-map' ></i></a>
+                </div>
+            </div>
+            <div className="second-part">
+                <p>be always withe news</p>
+                <input type="email" placeholder="youe email@luxe-scents"/> 
+                <button>submit</button>               
+            </div>
+            
+        </section>
+        <p className="cop">&copy; 2024 Your Company Name. All rights reserved.</p>
     </Fragment>
     );
 }
