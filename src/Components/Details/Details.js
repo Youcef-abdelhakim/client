3b98a7ec593850d10c1fdf3a6c675a3191fa0b64
youcef <abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.comgit config --global user.email abdelhakim1cef@gmail.com>
2024-09-26 05:21:57 -0700

    7

--- src/Components/Details/Details.js
+++ src/Components/Details/Details.js
@@ -9,10 +9,14 @@ const Details = () =>{
     useEffect(()=>{
         console.log(perfume);
     },[perfume])
+
+
+    const fstate = () =>{
+        return perfume.state === "In Stock"? "inf" : "outf";
+    }
     return(
-        <section className="delails-section">
-            <div className="principal-continer">
-                <div className="perf-details">
+        <section className="details-section">
+                <div className="title-details">
                     <h1>Details <span>About</span></h1>
                 </div>
                 <div className="prf-info">
@@ -20,14 +24,25 @@ const Details = () =>{
                     <img src={perfume.pictureUrl} alt={perfume.name}/>
                     </div>
                     <div className="fiche">
-                        <h2>{perfume.name}</h2>
-                        <h3>{perfume.brand}</h3>
-                        <h4>{perfume.state}</h4>
-                        <h4>{perfume.price}</h4>
-                        <p>{perfume.category}</p>
+                        <h2 className="ftitle">{perfume.name}</h2>
+                        <h3 className="fbrand">{perfume.brand}</h3>
+                        <p className="details-examplepr">Lorem ipsum dolor sit amet, consectetur 
+                            adipiscing elit. Sed do eiusmod tempor 
+                            incididunt ut labore et dolore magna
+                            aliqua. Ut enim ad minim veniam, quis
+                            nostrud exercitation ullamco laboris 
+                            nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in 
+                            voluptate velit esse cillum dolore eu 
+                            fugiat nulla pariatur. Excepteur sint
+                            occaecat cupidatat non proident, sunt in
+                            culpa qui officia deserunt mollit anim id est laborum.</p>
+
+                        <h4 className={fstate()}>{perfume.state}</h4>
+                        <h4 className="fprice">Price: {perfume.price} £</h4>
+                        <p className="fcateg">{perfume.category}</p>
                     </div>
                 </div>
-            </div>
         </section>
     );
 }
